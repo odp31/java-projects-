1ee89588f30e308b7bdd4f3432a1fc2c48f4d2e4
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-13 13:15:31 -0500

    Update searchAlgorithms.java

--- searchAlgorithms.java
+++ searchAlgorithms.java
@@ -34,3 +34,35 @@ public static int binarySearch(int[] arr, int key)
     }
   return -1; 
 }
+
+
+// 3. Jump Search: skips elements in blocks to reduce numComparisons; useful for large arrays where binary search is too slow 
+public static int jumpSearch(int[] arr, int target)
+  {
+  int n = arr.length;
+  int step = (int) Math.floor(Math.sqrt(n));
+  int prev = 0; 
+  while (arr[Math.min(step, n) - 1] < target)
+    {
+      prev = step; 
+      step += (int) Math.floor(Math.sqrt(n));
+
+      if (prev >= n)
+      {
+        return -1; 
+      }
+    }
+  while (arr[prev] < target)
+    {
+      prev++;
+      if(prev == Math.min(step, n))
+      {
+        return -1; 
+      }
+    }
+  if(arr[prev] == target)
+  {
+    return prev;
+  }
+  return -1;
+}
